8a626c071b908b5e9fefc0e809bc1e6a0bd580b7
NIKURE KETAKI SANTOSHRAO <133886525+KETAKISANTOSHNIKURE@users.noreply.github.com>
2023-12-15 19:02:39 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    // output: 'export',
+    images: {
+     
+      domains: ['lh3.googleusercontent.com'],
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    }
+  }
+  
+  module.exports = nextConfig
